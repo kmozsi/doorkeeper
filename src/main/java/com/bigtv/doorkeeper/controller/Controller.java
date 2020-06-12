715faf3a7dd4d0b53d7dddcaf1234ad24f049fd0
Gincsai Tibor <gincsait@gmail.com>
2020-06-12 19:45:41 +0200

    swagger codegen 02 - removed unnecessary definitions

--- src/main/java/com/bigtv/doorkeeper/controller/Controller.java
+++ src/main/java/com/bigtv/doorkeeper/controller/Controller.java
@@ -1,14 +1,10 @@
 package com.bigtv.doorkeeper.controller;
 
-import io.swagger.annotations.ApiParam;
 import org.openapitools.api.DoorApi;
-
 import org.openapitools.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @RestController
 public class Controller implements DoorApi {
@@ -20,44 +16,27 @@ public class Controller implements DoorApi {
 
 
     @Override
-    public ResponseEntity<EntryResponse> entry(
-        @ApiParam(value = "" ,required=true)
-        @RequestHeader(value="X-Token", required=true) String xToken
-    ) {
+    public ResponseEntity<EntryResponse> entry(String xToken) {
         return ResponseEntity.ok(new EntryResponse().permitted(true));
     }
 
     @Override
-    public ResponseEntity<Void> exit(
-        @ApiParam(value = "" ,required=true)
-        @RequestHeader(value="X-Token", required=true) String xToken
-    ) {
+    public ResponseEntity<Void> exit(String xToken) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<StatusResponse> getStatus(
-        @ApiParam(value = "" ,required=true)
-        @RequestHeader(value="X-Token", required=true) String xToken
-     ) {
+    public ResponseEntity<StatusResponse> getStatus(String xToken) {
         return ResponseEntity.ok(new StatusResponse().position(10));
     }
 
     @Override
-    public ResponseEntity<RegisterResponse> register(
-        @ApiParam(value = "" ,required=true)
-        @RequestHeader(value="X-Token", required=true) String xToken
-    ) {
+    public ResponseEntity<RegisterResponse> register(String xToken) {
         return ResponseEntity.ok(new RegisterResponse().accepted(true).position(10));
     }
 
     @Override
-    public ResponseEntity<Void> setCapacity(
-        @ApiParam(value = "" ,required=true)
-        @RequestHeader(value="X-Token", required=true) String xToken,
-        @ApiParam(value = "Optional description in *Markdown*" ,required=true)
-        @Valid @RequestBody CapacityBody capacityBody
-    ) {
+    public ResponseEntity<Void> setCapacity(String xToken, CapacityBody capacityBody) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
