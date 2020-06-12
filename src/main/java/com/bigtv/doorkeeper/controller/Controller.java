7e93acbd90b4f8f87edadb47c214b3daaf880e8c
Gincsai Tibor <gincsait@gmail.com>
2020-06-12 18:00:37 +0200

    swagger codegen 01

--- src/main/java/com/bigtv/doorkeeper/controller/Controller.java
+++ src/main/java/com/bigtv/doorkeeper/controller/Controller.java
@@ -1,13 +1,64 @@
 package com.bigtv.doorkeeper.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiParam;
+import org.openapitools.api.DoorApi;
+
+import org.openapitools.model.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
-public class Controller {
+public class Controller implements DoorApi {
 
     @GetMapping("/api")
     public String index() {
         return "Skeleton";
     }
+
+
+    @Override
+    public ResponseEntity<EntryResponse> entry(
+        @ApiParam(value = "" ,required=true)
+        @RequestHeader(value="X-Token", required=true) String xToken
+    ) {
+        return ResponseEntity.ok(new EntryResponse().permitted(true));
+    }
+
+    @Override
+    public ResponseEntity<Void> exit(
+        @ApiParam(value = "" ,required=true)
+        @RequestHeader(value="X-Token", required=true) String xToken
+    ) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<StatusResponse> getStatus(
+        @ApiParam(value = "" ,required=true)
+        @RequestHeader(value="X-Token", required=true) String xToken
+     ) {
+        return ResponseEntity.ok(new StatusResponse().position(10));
+    }
+
+    @Override
+    public ResponseEntity<RegisterResponse> register(
+        @ApiParam(value = "" ,required=true)
+        @RequestHeader(value="X-Token", required=true) String xToken
+    ) {
+        return ResponseEntity.ok(new RegisterResponse().accepted(true).position(10));
+    }
+
+    @Override
+    public ResponseEntity<Void> setCapacity(
+        @ApiParam(value = "" ,required=true)
+        @RequestHeader(value="X-Token", required=true) String xToken,
+        @ApiParam(value = "Optional description in *Markdown*" ,required=true)
+        @Valid @RequestBody CapacityBody capacityBody
+    ) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
