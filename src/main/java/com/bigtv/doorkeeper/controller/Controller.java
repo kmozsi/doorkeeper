0146ee804e31b0ae4e2fd9ccceee128572f8fc7f
horvath1att426 <horvath.attila1@telekom.hu>
2020-06-14 22:17:56 +0200

    Minor refactors

--- src/main/java/com/bigtv/doorkeeper/controller/Controller.java
+++ src/main/java/com/bigtv/doorkeeper/controller/Controller.java
@@ -3,23 +3,21 @@ package com.bigtv.doorkeeper.controller;
 import com.bigtv.doorkeeper.service.BookingService;
 import com.bigtv.doorkeeper.enumeration.Role;
 import com.bigtv.doorkeeper.service.JwtService;
+import lombok.extern.slf4j.Slf4j;
 import org.openapitools.api.DoorApi;
 import org.openapitools.model.CapacityBody;
 import org.openapitools.model.EntryResponse;
 import org.openapitools.model.RegisterResponse;
 import org.openapitools.model.StatusResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Controller implements DoorApi {
 
     private final BookingService bookingService;
     private final JwtService jwtService;
-    private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     public Controller(BookingService bookingService, JwtService jwtService) {
         this.bookingService = bookingService;
@@ -29,36 +27,36 @@ public class Controller implements DoorApi {
     @Override
     public ResponseEntity<EntryResponse> entry(String xToken) {
         String userId = jwtService.parseToken(xToken, Role.EMPLOYEE);
-        logger.info("Received entry call with userId: " + userId);
+        log.info("Received entry call with userId: " + userId);
         return ResponseEntity.ok(bookingService.entry(userId));
     }
 
     @Override
     public ResponseEntity<Void> exit(String xToken) {
         String userId = jwtService.parseToken(xToken, Role.EMPLOYEE);
-        logger.info("Received exit call with userId: " + userId);
+        log.info("Received exit call with userId: " + userId);
         bookingService.exit(userId);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
     @Override
     public ResponseEntity<StatusResponse> getStatus(String xToken) {
         String userId = jwtService.parseToken(xToken, Role.EMPLOYEE);
-        logger.info("Received get status call with userId: " + userId);
+        log.info("Received get status call with userId: " + userId);
         return ResponseEntity.ok(bookingService.status(userId));
     }
 
     @Override
     public ResponseEntity<RegisterResponse> register(String xToken) {
         String userId = jwtService.parseToken(xToken, Role.EMPLOYEE);
-        logger.info("Received register call with userId: " + userId);
+        log.info("Received register call with userId: " + userId);
         return ResponseEntity.ok(bookingService.register(userId));
     }
 
     @Override
     public ResponseEntity<Void> setCapacity(String xToken, CapacityBody capacityBody) {
         String userId = jwtService.parseToken(xToken, Role.EMPLOYEE, Role.HR);
-        logger.info("Received set capacity call with userId: " + userId);
-        return new ResponseEntity<>(HttpStatus.OK);
+        log.info("Received set capacity call with userId: " + userId);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/bigtv/doorkeeper/entity/OfficeCapacity.java
+++ src/main/java/com/bigtv/doorkeeper/entity/OfficeCapacity.java
@@ -12,11 +12,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 public class OfficeCapacity {
-  // TODO validate values and maybe the daily capacity (capacity * allowedPercentage / 100 could be cached)
+
+  // TODO Maybe the daily capacity (capacity * allowedPercentage / 100 could be cached)
   @Id
   @GeneratedValue
   private int id;
   private int capacity;
   private int allowedPercentage;
+
+  public void setCapacity(final int capacity) {
+    if (capacity < 0) {
+      throw new IllegalArgumentException("Cannot accept negative value as capacity!");
+    }
+    this.capacity = capacity;
+  }
+
+  public void setAllowedPercentage(final int allowedPercentage) {
+    if (allowedPercentage < 0 || allowedPercentage > 100) {
+      throw new IllegalArgumentException("Allowed percentage must be between 1 and 100!");
+    }
+    this.allowedPercentage = allowedPercentage;
+  }
 
 }
