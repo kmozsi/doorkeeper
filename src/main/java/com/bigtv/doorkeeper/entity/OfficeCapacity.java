22220179828d413d85d22c397931886ba81f2a7e
Gincsai Tibor <gincsait@gmail.com>
2020-06-14 10:52:15 +0200

    refactor 2

--- src/main/java/com/bigtv/doorkeeper/entity/OfficeCapacity.java
+++ src/main/java/com/bigtv/doorkeeper/entity/OfficeCapacity.java
@@ -3,12 +3,9 @@ package com.bigtv.doorkeeper.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Entity
 @Data
@@ -18,8 +15,8 @@ public class OfficeCapacity {
   // TODO validate values and maybe the daily capacity (capacity * allowedPercentage / 100 could be cached)
   @Id
   @GeneratedValue
-  private Integer id;
-  private Integer capacity;
-  private Integer allowedPercentage;
+  private int id;
+  private int capacity;
+  private int allowedPercentage;
 
 }
