de9a4b4ce40c90c379175c11cfb0eef11a028a33
horvath1att426 <horvath.attila1@telekom.hu>
2020-06-28 12:00:45 +0200

    Store current map of the office

---
+++ src/main/java/com/karanteam/doorkeeper/entity/Image.java
@@ -0,0 +1,30 @@
+package com.karanteam.doorkeeper.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Image {
+
+    public static final String OFFICE = "OFFICE";
+    public static final String OFFICE_STATUS = "OFFICE_STATUS";
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @Lob
+    @NonNull
+    private byte[] content;
+    @NonNull
+    private String key;
+
+}
