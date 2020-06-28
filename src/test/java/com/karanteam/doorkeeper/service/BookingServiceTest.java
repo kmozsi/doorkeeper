ace90b558925a44cf4be4b29c062218700a6c2f0
Krisztián Mózsi <mozsik0@gmail.com>
2020-06-28 12:41:25 +0200

    Add kafka messaging tests

--- src/test/java/com/karanteam/doorkeeper/service/BookingServiceTest.java
+++ src/test/java/com/karanteam/doorkeeper/service/BookingServiceTest.java
@@ -1,5 +1,6 @@
 package com.karanteam.doorkeeper.service;
 
+import com.karanteam.doorkeeper.config.MessagingConfig;
 import com.karanteam.doorkeeper.entity.Booking;
 import com.karanteam.doorkeeper.exception.EntryForbiddenException;
 import com.karanteam.doorkeeper.exception.EntryNotFoundException;
@@ -11,7 +12,10 @@ import org.openapitools.model.StatusResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -21,6 +25,7 @@ import static org.mockito.Mockito.*;
 public class BookingServiceTest {
 
     private static final String USER_ID = "USER_ID";
+    private static final String USER_ID2 = "USER_ID2";
 
     @Autowired
     private BookingService bookingService;
@@ -33,6 +38,12 @@ public class BookingServiceTest {
 
     @MockBean
     private BookingCacheService bookingCacheService;
+
+    @SpyBean
+    private MessagingConfig messagingConfig;
+
+    @MockBean
+    private MessagingService messagingService;
 
     private static final Booking waitingBooking = Booking.builder()
         .userId(USER_ID).ordinal(1).build();
@@ -50,11 +61,21 @@ public class BookingServiceTest {
 
     @Test
     public void callingExitWithRegularUser() {
+        List<Booking> bookings = new ArrayList<>();
+        bookings.add(Booking.builder().ordinal(0).userId(USER_ID).build());
+        bookings.add(Booking.builder().ordinal(1).userId(USER_ID2).build());
+
+        when(bookingCacheService.calculatePositionFromOrdinal(0)).thenReturn(0);
+        when(bookingCacheService.calculatePositionFromOrdinal(1)).thenReturn(1);
         when(vipService.isVip(anyString())).thenReturn(false);
+        when(bookingRepository.findAll()).thenReturn(bookings);
+        when(messagingConfig.getNotifyPosition()).thenReturn(1);
+        doNothing().when(messagingService).sendMessage(anyString());
 
         bookingService.exit(USER_ID);
 
         verify(bookingCacheService, times(1)).exit(USER_ID);
+        verify(messagingService, times(1)).sendMessage(USER_ID2);
     }
 
     @Test
